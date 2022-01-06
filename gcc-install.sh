863e7d120d25fdf2e8418153611bd0aac7c206e3
Piotro <piotro888@wp.pl>
2022-01-06 16:04:47 +0100

    Add gcc install script

---
+++ gcc-install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+echo "** starting gcc-pcpu installation"
+
+mkdir -p build/gcc
+
+#git clone https://github.com/gcc-mirror/gcc.git build/gcc
+echo "> Cloning gcc compiler"
+git clone -b releases/gcc-8 --depth 1 https://github.com/gcc-mirror/gcc.git build/gcc 
+cd build/gcc
+
+git checkout releases/gcc-8.5.0
+
+echo "> Applying patches"
+cp -rv ../../gcc-pcpu/* .
+
+cd ..
+#rm -r gcc-obj
+mkdir gcc-obj
+cd gcc-obj
+
+# uncomment if needed
+[ -f /usr/bin/x86_64-pc-linux-gnu-gcc-ar ] && export AR=/usr/bin/x86_64-pc-linux-gnu-gcc-ar
+
+echo "> Building gcc"
+pcpu_triple=pcpu-unknown-elf
+host_triple=$(gcc -dumpmachine)
+bin_prefix=$HOME/opt/gcc-pcpu2
+
+if [ -d bin_prefix ]; then
+    rm -rv $bin_prefix
+    echo "> Cleaned old install directory"
+fi
+
+mkdir -p $bin_prefix
+../gcc/configure --prefix=$bin_prefix --enable-languages=c,c++ --build=$host_triple --target=$pcpu_triple --host=$host_triple
+
+if [ $? -ne 0 ]; then
+    echo "*** CONFIGURE FAILED"
+    exit 1
+fi
+
+# remember to rm -rf build/gcc-obj if you change someting here
+
+echo "make"
+make -j 6
+echo "make install"
+make install
+
+echo "NOTE: make install build should end with error"
+echo "> Testing gcc now: (build/test/)"
+
+mkdir -p test
+cd test
+# run simple tests
+echo "int main() {int x = 1+2; return x;}" > test.c
+$bin_prefix/bin/$pcpu_triple-gcc -S test.c
+rc=$?
+
+if [ $rc -ne 0 ] || [ ! -f test.s ] ; then
+    echo " *** TEST FAILED! Cleaning obj dir. Try to re-run or rm -rf build/, fetch new version or contact repo owner";
+    rm -r gcc-obj
+    exit 1
+fi
+
+echo "> Compile test passed"
+echo "NOTE: gcc is located at $bin_prefix/bin/$pcpu_triple-gcc"
+echo "** gcc installation finished!"
